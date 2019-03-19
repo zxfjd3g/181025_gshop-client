4220ef1e5f975c688670ee7ba767cab39052ec20
zxfjd3g <258147149@qq.com>
2019-03-19 16:38:09 +0800

    form

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -19,7 +19,7 @@
               </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码">
+              <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
             </section>
             <section class="login_hint">
               温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
@@ -29,22 +29,22 @@
           <div :class="{on: !loginWay}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                <input type="tel" maxlength="11" placeholder="用户名" v-model="name">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="isShowPwd ? 'text' : 'password'" maxlength="8" placeholder="密码" v-model="pwd">
+                <div class="switch_button" :class="isShowPwd ? 'on' : 'off'" @click="isShowPwd = !isShowPwd">
+                  <div class="switch_circle" :class="{right: isShowPwd}"></div>
+                  <span class="switch_text">{{isShowPwd ? 'abc' : ''}}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码">
+                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                 <img class="get_verification" src="./images/captcha.svg" alt="captcha">
               </section>
             </section>
           </div>
-          <button class="login_submit">登录</button>
+          <button class="login_submit" @click.prevent="login">登录</button>
         </form>
         <a href="javascript:;" class="about_us">关于我们</a>
       </div>
@@ -58,9 +58,14 @@
   export default {
     data () {
       return {
-        loginWay: true, // true: 短信, false: 密码
+        loginWay: false, // true: 短信, false: 密码
         phone: '', // 手机号
+        code: '', // 短信验证码
+        name: '', // 用户名
+        pwd: '',  // 密码
+        captcha: '', // 图形验证码
         computeTime: 0, // 倒计时剩余的时间
+        isShowPwd: false, // 是否显示密码
       }
     },
 
@@ -72,6 +77,9 @@
     },
 
     methods: {
+      /*
+      发送验证 + 倒计时
+       */
       sendCode () {
         // alert('sendCode')
 
@@ -85,6 +93,32 @@
             clearInterval(intervalId)
           }
         }, 1000)
+
+      },
+
+      /*
+      请求登陆 + 前台表单验证
+       */
+      login () {
+        // 取出表单收集数据
+        const {loginWay, isRightPhone, phone, code, name, pwd, captcha} = this
+        if(loginWay) { // 短信
+          if(!isRightPhone) {
+            return alert('请输入正确的手机号')
+          } else if (!/^\d{6}$/.test(code)) {
+            return alert('验证码必须是6位数字')
+          }
+        } else { // 密码
+          if(!name.trim()) {
+            alert('必须指定用户名')
+          } else if (!pwd.trim()) {
+            alert('必须指定密码')
+          } else if (!/^.{4}$/.test(captcha)) {
+            alert('必须指定4位验证码')
+          }
+        }
+
+        // 发送登陆的ajax请求
 
       }
     }
@@ -182,7 +216,6 @@
                 &.on
                   background #02a774
                 >.switch_circle
-                //transform translateX(27px)
                   position absolute
                   top -1px
                   left -1px
@@ -193,6 +226,8 @@
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                   transition transform .3s
+                  &.right
+                    transform translateX(27px)
             .login_hint
               margin-top 12px
               color #999
